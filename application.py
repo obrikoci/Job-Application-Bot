95fdb3e537ece3a2c2e439f4f22af0b9797065c7
Obri Koci <obrikoci2@gmail.com>
2024-07-12 23:32:48 +0200

    initial commit

---
+++ application.py
@@ -0,0 +1,82 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.common.by import By
+# import time
+#
+# ACCOUNT_EMAIL = "geaa00643@gmail.com"
+# ACCOUNT_PASSWORD = "tirana 1"
+# PHONE = "123456789"
+#
+#
+# def abort_application():
+#     # Click Close Button
+#     close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+#     close_button.click()
+#
+#     time.sleep(2)
+#     # Click Discard Button
+#     discard_button = driver.find_elements(by=By.CLASS_NAME, value="artdeco-modal__confirm-dialog-btn")[1]
+#     discard_button.click()
+#
+#
+# # Optional - Keep the browser open if the script crashes.
+# chrome_opt = webdriver.ChromeOptions()
+# chrome_opt.add_experimental_option("detach", True)
+#
+# driver = webdriver.Chrome(options=chrome_opt)
+#
+# driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3586148395&f_LF=f_AL&geoId=101356765&"
+#            "keywords=python&location=London%2C%20England%2C%20United%20Kingdom&refresh=true")
+#
+# # Click Sign in Button
+# time.sleep(2)
+# sign_in_button = driver.find_element(by=By.LINK_TEXT, value="Sign in")
+# sign_in_button.click()
+#
+# # Sign in
+# time.sleep(5)
+# email_field = driver.find_element(by=By.ID, value="username")
+# email_field.send_keys(ACCOUNT_EMAIL)
+# password_field = driver.find_element(by=By.ID, value="password")
+# password_field.send_keys(ACCOUNT_PASSWORD, Keys.ENTER)
+#
+# # CAPTCHA - Solve Puzzle Manually
+# input("Press Enter after solving the Captcha")
+#
+# # Get Listings
+# time.sleep(5)
+# job_listings = driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
+#
+# # Apply for Jobs
+# for listing in job_listings:
+#     listing.click()
+#     time.sleep(2)
+#     try:
+#         apply_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
+#         apply_button.click()
+#
+#         time.sleep(5)
+#         phone = driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
+#         if phone.text == "":
+#             phone.send_keys(PHONE)
+#
+#         submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
+#         if submit_button.get_attribute("data-control-name") == "continue_unify":
+#             abort_application()
+#             print("Complex application, skipped.")
+#             continue
+#         else:
+#             submit_button.click()
+#
+#         time.sleep(2)
+#         close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+#         close_button.click()
+#
+#     except NoSuchElementException:
+#         abort_application()
+#         print("No application button, skipped.")
+#         continue
+#
+# time.sleep(5)
+# driver.quit()
---
+++ save&follow.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+
+
+chrome_opt = webdriver.ChromeOptions()
+chrome_opt.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_opt)
+driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&"
+           "keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
+
+time.sleep(2)
+sign_in = driver.find_element(By.LINK_TEXT, "Sign in")
+sign_in.click()
+
+time.sleep(2)
+email_field = driver.find_element(By.ID, "username")
+email_field.send_keys("geaa00643@gmail.com")
+password_field = driver.find_element(By.ID, "password")
+password_field.send_keys("tirana 1", Keys.ENTER)
+time.sleep(2)
+input("Press Enter after solving the Captcha")
+
+job_listings = driver.find_elements(By.CSS_SELECTOR, ".job-card-container--clickable")
+for job in job_listings:
+    job.click()
+    time.sleep(2)
+
+    save_button = driver.find_element(By.CSS_SELECTOR, ".jobs-save-button span")
+    save_button.click()
+    time.sleep(2)
+
+    wait = WebDriverWait(driver, 10)
+    try:
+        follow_button = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, ".follow.artdeco-button.artdeco-button--secondary.ml5")))
+
+        # Scroll into view
+        driver.execute_script("arguments[0].scrollIntoView(true);", follow_button)
+
+        # Attempt to click the element using JavaScript
+        driver.execute_script("arguments[0].click();", follow_button)
+        print("Follow button clicked using JavaScript")
+
+    except NoSuchElementException:
+        print("Follow button not found")
+    except ElementClickInterceptedException:
+        print("Element click intercepted")
+
+    time.sleep(2)
+driver.quit()
